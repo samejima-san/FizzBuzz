ee4fcadaf444c5aea7c11b2d305c5d4a2c3bb289
Samejima <46758064+samejima-san@users.noreply.github.com>
2021-06-25 17:15:39 +0000

    fizzbuzz cringe

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // if the number is divisible by 5 prints Buzz
 // divisible by 3 && 5 print out FizzBuzz
 // otherwise, prints the number
-let fizzbuzz = (fizz = 3, buzz = 5, length = 16) => {
+let fizzbuzz = (fizz = 3, buzz = 5, length = 100) => {
   for (let i = 0; i <= length; i++) {
     if ((i % fizz === 0) && (i % buzz === 0)) {
       console.log("FizzBuzz");
